0033928eaff73eacd283ce30cd9f84d3a4b741f0
PaCo <PaCo@WS-2669.profidata.com>
2016-08-30 11:20:55 +0200

    removed commented code.

--- com.profidatagroup.e4.advancedlaunch/src/com/profidatagroup/e4/advancedlaunch/strategies/AbstractLaunchStrategy.java
+++ com.profidatagroup.e4.advancedlaunch/src/com/profidatagroup/e4/advancedlaunch/strategies/AbstractLaunchStrategy.java
@@ -14,52 +14,7 @@ import org.eclipse.debug.core.model.IProcess;
 import com.profidatagroup.e4.advancedlaunch.tabs.SampleTab;
 
 public abstract class AbstractLaunchStrategy {
-	
-	private static ILaunch childLaunch;
-	
-
-
-		
-//		ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
-//		ILaunchConfigurationType type = manager
-//				.getLaunchConfigurationType("org.eclipse.jdt.launching.localJavaApplication");
-//		ILaunchConfiguration[] configurations;
-
-//		useOptionalProcessTerminatedListener();
-//		launchSelectedStrategy();
-		
-		// all configurations of "Java Application"
-//		try {
-//			configurations = manager.getLaunchConfigurations(type);
-//
-//			for (int i = 0; i < SampleTab.launchConfigurationDataList.size(); i++) {
-//				for (int j = 0; j < configurations.length; j++) {
-//					if (SampleTab.launchConfigurationDataList.get(i).getName().equals(configurations[j].getName())) {
-//						childLaunch = configurations[j].launch("debug", null);
-//						launchSelectedStrategy();
-//						System.out.println(SampleTab.launchConfigurationDataList.get(i) + " was launched!");
-//					}
-//				}
-//			}
-//		} catch (CoreException e) {
-//			e.printStackTrace();
-//		}
-	
 
 	protected abstract void launchSelectedStrategy(ILaunchConfiguration iLaunchConfiguration, String mode, String param);
-
-	/*
-	 * <b>ONLY</b> class <b>WaitUntilPriorConfigTerminatedStrategy</b> should override this
-	 * method.
-	 * 
-	 * 
-	 */
-//	protected void useOptionalProcessTerminatedListener() {
-//		
-//	}
-	
-	protected static ILaunch getChildLaunch() {
-		return childLaunch;
-	}
 
 }
