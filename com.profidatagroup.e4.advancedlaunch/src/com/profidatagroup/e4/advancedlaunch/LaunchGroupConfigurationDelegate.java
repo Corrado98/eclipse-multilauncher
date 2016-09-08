cdf5bafe0c66b94a002584acf787f3c5f457bfd9
PaCo <PaCo@WS-2669.profidata.com>
2016-09-08 17:17:33 +0200

    implemented infinite loop detection

--- com.profidatagroup.e4.advancedlaunch/src/com/profidatagroup/e4/advancedlaunch/LaunchGroupConfigurationDelegate.java
+++ com.profidatagroup.e4.advancedlaunch/src/com/profidatagroup/e4/advancedlaunch/LaunchGroupConfigurationDelegate.java
@@ -2,27 +2,40 @@ package com.profidatagroup.e4.advancedlaunch;
 
 import java.util.List;
 
+import org.eclipse.cdt.launch.internal.ui.LaunchMessages;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.PlatformUI;
 
 import com.profidatagroup.e4.advancedlaunch.strategies.AbstractLaunchStrategy;
 import com.profidatagroup.e4.advancedlaunch.strategies.DelayStrategy;
 import com.profidatagroup.e4.advancedlaunch.strategies.EmptyStrategy;
 import com.profidatagroup.e4.advancedlaunch.strategies.ReadConsoleTextStrategy;
 import com.profidatagroup.e4.advancedlaunch.strategies.WaitForTerminationStrategy;
-import com.profidatagroup.e4.advancedlaunch.tabs.LaunchTab;
 
 public class LaunchGroupConfigurationDelegate implements ILaunchConfigurationDelegate {
-
+	
+	private boolean infiniteLoopDetection = true;
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
 			throws CoreException {
 
 		List<LaunchConfigurationBean> launchConfigurationDataList = LaunchUtils.loadLaunchConfigurations(configuration);
+		
+		if(isInfiniteLoop(configuration, launch, launchConfigurationDataList)) {
+			if(infiniteLoopDetection) {
+			showInfiniteLoopErrorMessage(configuration);
+			infiniteLoopDetection = false;
+			}
+			return;
+		}
 
 		for (LaunchConfigurationBean bean : launchConfigurationDataList) {
 			ILaunchConfiguration launchConfiguration = LaunchUtils.findLaunchConfiguration(bean.getName());
@@ -32,7 +45,7 @@ public class LaunchGroupConfigurationDelegate implements ILaunchConfigurationDel
 			}
 		}
 	}
-	
+
 	private AbstractLaunchStrategy createLaunchAndWaitStrategy(LaunchConfigurationBean launchConfigurationBean) {
 		switch (launchConfigurationBean.getPostLaunchAction()) {
 		case "Wait until terminated":
@@ -50,5 +63,27 @@ public class LaunchGroupConfigurationDelegate implements ILaunchConfigurationDel
 
 		throw new IllegalArgumentException(
 				"Unknown launch and wait strategy: " + launchConfigurationBean.getPostLaunchAction());
+	}
+	
+	private boolean isInfiniteLoop(ILaunchConfiguration configuration, ILaunch launch,
+			List<LaunchConfigurationBean> launchConfigurationDataList) throws DebugException {
+		for (LaunchConfigurationBean bean : launchConfigurationDataList) {
+			if (configuration.getName().equals(bean.getName())) {	
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void showInfiniteLoopErrorMessage(ILaunchConfiguration configuration) {
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+						LaunchMessages.LaunchUIPlugin_Error,
+						NLS.bind(LaunchMessages.MultiLaunchConfigurationDelegate_Loop,
+								configuration.getName()));
+			}
+		});
 	}
 }
