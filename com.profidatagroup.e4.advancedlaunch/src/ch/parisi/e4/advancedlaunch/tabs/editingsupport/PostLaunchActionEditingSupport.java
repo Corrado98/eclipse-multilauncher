69a17fc44e129de2cccfdbd54cd74c0f3d5ef65c
Corrado98 <corradoparisi@hotmail.com>
2016-09-30 08:52:05 +0200

    fix postLaunchAction editing support ComboBox

--- com.profidatagroup.e4.advancedlaunch/src/ch/parisi/e4/advancedlaunch/tabs/editingsupport/PostLaunchActionEditingSupport.java
+++ com.profidatagroup.e4.advancedlaunch/src/ch/parisi/e4/advancedlaunch/tabs/editingsupport/PostLaunchActionEditingSupport.java
@@ -1,20 +1,13 @@
 package ch.parisi.e4.advancedlaunch.tabs.editingsupport;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ComboBoxCellEditor;
 import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.TableViewer;
 
 import ch.parisi.e4.advancedlaunch.LaunchConfigurationModel;
-import ch.parisi.e4.advancedlaunch.utils.PostLaunchAction;
 import ch.parisi.e4.advancedlaunch.utils.PostLaunchActionUtils;
 
 public class PostLaunchActionEditingSupport extends EditingSupport {
@@ -45,24 +38,9 @@ public class PostLaunchActionEditingSupport extends EditingSupport {
 	@Override
 	protected void setValue(Object element, Object value) {
 		LaunchConfigurationModel launchConfigurationModel = (LaunchConfigurationModel) element;
-		launchConfigurationModel.setName(getAllLaunchConfigurations().get((int) value));
 		launchConfigurationModel.setPostLaunchAction(PostLaunchActionUtils.convertToPostLaunchAction(
 				Arrays.asList(PostLaunchActionUtils.getPostLaunchActionNames()).get((int) value)));
+		
+		tableViewer.update(launchConfigurationModel, null);
 	}
-
-	private static List<String> getAllLaunchConfigurations() {
-		List<String> allLaunchConfigurations = new ArrayList<>();
-
-		ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
-		try {
-			for (ILaunchConfiguration configuration : manager.getLaunchConfigurations()) {
-				allLaunchConfigurations.add(configuration.getName());
-			}
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
-
-		return allLaunchConfigurations;
-	}
-
 }
