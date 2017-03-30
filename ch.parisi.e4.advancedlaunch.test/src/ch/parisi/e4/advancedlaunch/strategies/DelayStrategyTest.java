d05edac3824434f8134020668b0a62eefae0105e
Corrado98 <corradoparisi@hotmail.com>
2017-03-30 10:30:44 +0200

    add DSL for delay strategy

--- ch.parisi.e4.advancedlaunch.test/src/ch/parisi/e4/advancedlaunch/strategies/DelayStrategyTest.java
+++ ch.parisi.e4.advancedlaunch.test/src/ch/parisi/e4/advancedlaunch/strategies/DelayStrategyTest.java
@@ -1,53 +1,50 @@
 package ch.parisi.e4.advancedlaunch.strategies;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
 
-import ch.parisi.e4.advancedlaunch.strategies.DelayStrategy;
-import ch.parisi.e4.advancedlaunch.strategies.WaitStrategy;
-
 /**
- * The {@link DelayStrategyTest}.
+ * Tests {@link DelayStrategy}.
  */
 public class DelayStrategyTest {
 
 	/**
-	 * This method tests a basic delay strategy.
+	 * Tests a basic {@link DelayStrategy}.
 	 * @throws InterruptedException if any thread has interrupted the current thread.
 	 */
 	@Test
 	public void basicDelayStrategyTest() throws InterruptedException {
-		long delayInMilliseconds = 1000;
-		WaitStrategy delayStrategy = new DelayStrategy((int) delayInMilliseconds / 1000);
-		AtomicLong actualDelayInMilliseconds = new AtomicLong(0);
-
-		Thread delayStrategyThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				long startTime = System.currentTimeMillis();
-				delayStrategy.waitForLaunch(null);
-				long endTime = System.currentTimeMillis();
-
-				actualDelayInMilliseconds.set(endTime - startTime);
-			}
-		});
-		delayStrategyThread.start();
-		delayStrategyThread.join();
-
-		assertEquals(delayInMilliseconds, actualDelayInMilliseconds.get(), 100);
+		assertEquals(1000, measureDelayStrategy(1000), 100);
 	}
 
 	/**
-	 * 
-	 * 
+	 * Tests that a {@link DelayStrategy} stops waiting if the launch terminates normally. 
 	 * @throws InterruptedException if any thread has interrupted the current thread.
 	 */
 	@Test
-	public void stopWaitingWhenLaunchTerminatedTest() throws InterruptedException {
-		long delayInMilliseconds = 10000;
+	public void stopWaitingWhenLaunchTerminatesWithoutErrorTest() throws InterruptedException {
+		assertTrue(measureDelayStrategy(10000, 0) < 1100);
+	}
+
+	/**
+	 * Tests that a {@link DelayStrategy} stops waiting if the launch terminates with 
+	 * an exit code other than zero. 
+	 * @throws InterruptedException if any thread has interrupted the current thread.
+	 */
+	@Test
+	public void stopWaitingWhenLaunchTerminatesWithErrorTest() throws InterruptedException {
+		assertTrue(measureDelayStrategy(10000, 99) < 1100);
+	}
+
+	private long measureDelayStrategy(long delayInMilliseconds) throws InterruptedException {
+		return measureDelayStrategy(delayInMilliseconds, null);
+	}
+
+	private long measureDelayStrategy(long delayInMilliseconds, Integer exitCode) throws InterruptedException {
 		WaitStrategy delayStrategy = new DelayStrategy((int) delayInMilliseconds / 1000);
 		AtomicLong actualDelayInMilliseconds = new AtomicLong(0);
 
@@ -62,11 +59,13 @@ public class DelayStrategyTest {
 			}
 		});
 		delayStrategyThread.start();
+		if (exitCode != null) {
+			delayStrategy.launchTerminated(exitCode);
+		}
 
 		delayStrategyThread.join();
 
-		assertEquals(delayInMilliseconds, actualDelayInMilliseconds.get(), 5);
-
+		return actualDelayInMilliseconds.get();
 	}
 
 }
