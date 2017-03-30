bdadfbf1df5414b15860a61c6702ea0e2f9958e2
Corrado98 <corradoparisi@hotmail.com>
2017-03-30 13:24:11 +0200

    add wait for termination strategy test

---
+++ ch.parisi.e4.advancedlaunch.test/src/ch/parisi/e4/advancedlaunch/strategies/WaitForTerminationStrategyTest.java
@@ -0,0 +1,50 @@
+package ch.parisi.e4.advancedlaunch.strategies;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link WaitForTerminationStrategy}.
+ */
+public class WaitForTerminationStrategyTest {
+
+	/**
+	 * Tests a basic {@link WaitForTerminationStrategy}.
+	 * @throws InterruptedException if any thread has interrupted the current thread. 
+	 */
+	@Test
+	public void basicWaitForTerminationStrategyTest() throws InterruptedException {
+		WaitForTerminationStrategy waitForTerminationStrategy = new WaitForTerminationStrategy();
+		AtomicLong actualDelayInMilliseconds = new AtomicLong(0);
+
+		Thread waitForTerminationStrategyThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				sleep(2000);
+				waitForTerminationStrategy.launchTerminated(0);
+			}
+		});
+
+		waitForTerminationStrategyThread.start();
+		long startTime = System.currentTimeMillis();
+		waitForTerminationStrategy.waitForLaunch(null);
+		long endTime = System.currentTimeMillis();
+
+		actualDelayInMilliseconds.set(endTime - startTime);
+
+		assertTrue(actualDelayInMilliseconds.get() < 3000);
+	}
+
+	private void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		}
+		catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
